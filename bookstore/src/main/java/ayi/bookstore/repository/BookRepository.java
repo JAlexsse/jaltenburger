c759c8b932a00c1cf3a974ce8e227b1ddb1fa4e6
JAlexsse <de.lite.alexssa@gmail.com>
2021-04-05 17:00:27 -0300

    repository test

--- bookstore/src/main/java/ayi/bookstore/repository/BookRepository.java
+++ bookstore/src/main/java/ayi/bookstore/repository/BookRepository.java
@@ -1,10 +1,16 @@
 package ayi.bookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ayi.bookstore.entity.Book;
 
 @Repository
-public interface BookRepository extends JpaRepository<Book, Integer>{   
+public interface BookRepository extends JpaRepository<Book, Integer>, JpaSpecificationExecutor<Book>{   
+
+    @Query(value = "SELECT * FROM BOOK WHERE BOOK_NAME = ?1", nativeQuery = true)
+    public Book findByBookName(String book_name);
+
 }
--- bookstore/src/main/java/ayi/bookstore/specification/BookSpecification.java
+++ bookstore/src/main/java/ayi/bookstore/specification/BookSpecification.java
@@ -2,6 +2,7 @@ package ayi.bookstore.specification;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Path;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
@@ -12,19 +13,19 @@ import ayi.bookstore.entity.Book;
 
 public class BookSpecification {
 
-    public Specification<Book> priceOnRange(Double price){
-        
+    public Specification<Book> getUserSpecificationByAuthorOrPublishing(String attribute, int valueAttribute){
         return new Specification<Book>() {
 
             private static final long serialVersionUID = 1L;
 
-            @Override
+            @Override 
             public Predicate toPredicate(Root<Book> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
-
-                return cb.lessThan(root.get("price"), price);
-            }
-
+            
+            Path<Book> path = root.get(attribute);
+            return cb.equal(path, valueAttribute);
+          };
         };
     }
+    
     
 }
